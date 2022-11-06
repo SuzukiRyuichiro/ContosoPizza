f531dea60bc6e66e8fd6b03f194d47c3d9465005
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2022-11-06 18:25:32 +0100

    feat: added  a pizza service

---
+++ Services/PizzaSevice.cs
@@ -0,0 +1,57 @@
+// this is like the repository in our Cookbook exercise
+using ContosoPizza.Models;
+
+namespace ContosoPizza.Services;
+
+public static class PizzaService
+{
+    static List<Pizza> Pizzas { get; }
+    static int nextId = 3;
+
+    static PizzaService()
+    // Static Constructor
+    {
+        Pizzas = new List<Pizza>
+        {
+            new Pizza
+            {
+                Id = 1,
+                Name = "Pepperoni",
+                IsGlutenFree = false
+            },
+            new Pizza
+            {
+                Id = 2,
+                Name = "Cheese",
+                IsGlutenFree = true
+            }
+        };
+    }
+
+    public static List<Pizza> GetAll() => Pizzas;
+
+    public static Pizza? Get(int id) => Pizzas.FirstOrDefault(pizza => pizza.Id == id);
+
+    public static void Add(Pizza pizza)
+    {
+        pizza.Id = nextId++;
+        Pizzas.Add(pizza);
+    }
+
+    public static void Delete(int id)
+    {
+        var pizza = Get(id);
+        if (pizza is null)
+            return;
+
+        Pizzas.Remove(pizza);
+    }
+
+    public static void Update(Pizza pizza)
+    {
+        var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
+        if (index == -1)
+            return;
+        Pizzas[index] = pizza;
+    }
+}
