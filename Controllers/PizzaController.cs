856ec9b631f324b790f320e69015acea05b92a6b
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2022-11-06 19:42:51 +0100

    basic crud done

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -25,7 +25,45 @@ public class PizzaController : ControllerBase
             return NotFound();
         return pizza;
     }
+
+    // POST action
+
+    [HttpPost]
+    public IActionResult Create(Pizza pizza)
+    {
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
+    }
+
     // PUT action
+    [HttpPut(template: "{id}")]
+    public IActionResult Update(int id, Pizza pizza)
+    {
+        // This code will update the pizza and return a result
+        if (id != pizza.Id)
+            return BadRequest();
+
+        // get the pizza instance with given id
+        var existingPizza = PizzaService.Get(id);
+        if (existingPizza is null)
+            return NotFound();
+
+        PizzaService.Update(pizza);
+
+        return NoContent();
+    }
 
     // DELETE action
+    [HttpDelete(template: "{id}")]
+    public IActionResult Delete(int id)
+    {
+        // This code will delete the pizza and return a result
+        var pizzaToDelete = PizzaService.Get(id);
+        if (pizzaToDelete is null)
+            return NotFound();
+
+        PizzaService.Delete(id);
+
+        return NoContent();
+    }
 }
